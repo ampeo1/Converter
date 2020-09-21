303b1842124b920a40d393df8ec9cc07b7a314f3
ampeo <ampeo1@outlook.com>
2020-09-21 22:12:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/laba1/Fields.java
@@ -0,0 +1,34 @@
+package com.example.laba1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class Fields extends Fragment {
+
+    private FieldsViewModel mViewModel;
+
+    public static Fields newInstance() {
+        return new Fields();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fileds_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FieldsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/laba1/Keyboard.java
@@ -0,0 +1,34 @@
+package com.example.laba1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class Keyboard extends Fragment {
+
+    private KeyboardViewModel mViewModel;
+
+    public static Keyboard newInstance() {
+        return new Keyboard();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.keyboard_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(KeyboardViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
